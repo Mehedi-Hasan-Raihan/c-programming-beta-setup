bfe30a438cee1739cab42d0cc626bac23a961092
Mehedi Hasan <mhr7official@gmail.com>
2023-04-05 12:33:56 +0600

    setup beta

---
+++ README.md
@@ -0,0 +1,1 @@
+## c-programming-beta-setup course
---
+++ setup/hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("helnnnnlo kittu man\n");
+    printf("superman");
+
+    printf(" ,mxzu macjkdscjsjcsdjn\n");
+    printf("superman\n");
+    printf("superman\n");
+    printf("superman\n");
+    printf("superman\n");
+    printf("superman\n");
+    return 0;
+}
